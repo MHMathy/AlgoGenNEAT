3a47625a0de50572399ca885e983e2d2e050d99e
Aurélien <aurelien99.martinet@gmail.com>
2019-09-28 12:03:43 +0200

    appli rotate

--- vroom/main.py
+++ vroom/main.py
@@ -19,9 +19,11 @@ background = pygame.transform.scale(background,(1050,750))
 ImageCar = pygame.image.load("voiture.jpg")
 ImageCar.set_colorkey(white)
 ImageCar = pygame.transform.scale(ImageCar,(75,50))
+car_rect = ImageCar.get_rect()
 
 def main():
     c = car()
+    car_rect.center = (c.pos[0], c.pos[1])
     tmp = 0
     window = False
     while window == False:
@@ -33,15 +35,17 @@ def main():
                 quit()
 
         if tmp > 1000 :
-            print(c.pos[0],c.pos[1])
             c.moove([c.pos[0]-50, c.pos[1]+25])
             global ImageCar
             ImageCar = pygame.transform.rotate(ImageCar, 45)
+            global car_rect
+            car_rect = ImageCar.get_rect()
+            car_rect.center = (c.pos[0],c.pos[1])
             tmp = 0
             
 
         surface.blit(background,(0,0))
-        surface.blit(ImageCar,(c.pos[0], c.pos[1]))
+        surface.blit(ImageCar,car_rect)
         pygame.display.update()
 
 
