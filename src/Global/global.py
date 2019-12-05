222d06d412145ef8ddb922b061350105595411af
MATHY MICHEL p1711015 <michel.mathy@etu.univ-lyon1.fr>
2019-12-05 11:37:54 +0100

    finition de la class global

--- src/Global/global.py
+++ src/Global/global.py
@@ -1,32 +1,82 @@
+"""
+separer calcul affichage
+import global dans des sous classes
+utilité sigmoid que si on fait varier l'intensité de l'action
+utiliser private ou non
+qu'est-ce qu'il y a dans le rapport
+
+"""
+
+"""
+TAILLE_POPULATION = 10
+
+DISTANCE_MIN_ESPECE = 5
+PROBA_MUTATION_GENOME = 50
+PROBA_AJOUT_CONNEC_GENOME = 10
+PROBA_AJOUT_NOEUD_GENOME = 10
+
+PROBA_MUTATION = 80
+PROBA_MUTATION_COEF = 90
+DISTANCE_C1 = 1
+DISTANCE_C2 = 1
+DISTANCE_C3 = 0.4
+DEFAULT_N_CONNEC = 6
+
+COEF_EXPO = 1
+"""
+
+
+
 class Global:
+
+    Cons = {
+        "TAILLE_POPULATION": 10,
+
+        "DISTANCE_MIN_ESPECE": 5,
+        "PROBA_MUTATION_GENOME": 50,
+        "PROBA_AJOUT_CONNEC_GENOME": 10,
+        "PROBA_AJOUT_NOEUD_GENOME": 10,
+
+        "PROBA_MUTATION": 80,
+        "PROBA_MUTATION_COEF": 90,
+        "DISTANCE_C1": 1,
+        "DISTANCE_C2": 1,
+        "DISTANCE_C3": 0.4,
+        "DEFAULT_N_CONNEC": 6
+
+        "COEF_EXPO": 1
+       }
+
     def __init__(self):
-        self.listeConstantes = {"proba_mut": Genome.PROBA_MUTATION,
-                           "proba_mut_coef": Genome.PROBA_MUTATION_COEF,
-                           "C1": Genome.DISTANCE_C1,
-                           "C2": Genome.DISTANCE_C2,
-                           "C3": Genome.DISTANCE_C3,
-                           "default_connec": Genome.DEFAULT_N_CONNEC}
-        self.population
+
         self.debutCycle
         self.dureeCycle
         self.generateurGenome
         self.listeVoiture = []
         self.dictGenScore = {}
-        self.dict
+
+        self.listMeilleurScore = []
 
     def demarreProgramme(self,population):
-
-        self.population = population
-        self.listeVoiture = [voiture(Genome.default_mini(),190,110)]*population
         self.generateurGenome = Generation()
-        self.debutCycle = time.time()
+        Global.Cons["TAILLE_POPULATION"] = population
+        self.listeVoiture = [voiture(Genome.default_mini(),190,110)]*population
+        for v in self.listeVoiture:
+            dictGenScore.update({v.genome :0})
+
 
 
     def demarreCycle(self):
+
+        self.generateurGenome.evaluer(self.dictGenScore)
+        if len(self.generateurGenome.get_listGenomes())!= Global.Cons.get("TAILLE_POPULATION"):
+            print("MEGA GROSSE ERREUR")
+        for gen in self.generateurGenome.get_listGenomes():
+            self.listeVoiture.append(voiture(gen,190,110))
+
+        self.dictGenScore.clear()
+
         self.debutCycle = time.time()
-        #generateurGenome créer new Gen
-        #init les voitures avec
-        #recommence le cycle
 
 
     #plus tard update avec un while et affichge en thread
@@ -38,8 +88,17 @@ class Global:
 
 
     def fin_cycle(self):
-        self.dictGenScore = {}
+
         for v in self.listeVoiture:
             dictGenScore.update({v.genome : v.calculscore()})
 
-    #fontion pour distribuer les variables globales
+        self.listMeilleurScore.append(max(self.dictGenScore;values()))
+        self.listeVoiture.clear()
+
+    @classmethod
+    def get_listConstantes(cls):
+        return cls.Cons
+
+    @classmethod
+    def set_listConstantes(cls,nomConst,valConst):
+            cls.Cons[nomConst] = valConst
